c396e88e4798ae7008dc3c937a298fe3b53ddbf6
ryanseraj <seraj.y.rayan@hotmail.com>
2023-01-11 19:57:45 +0200

    EDaproxemation

--- implementations/try1.py
+++ implementations/try1.py
@@ -1,9 +1,10 @@
 ############### HELPER FUNCTIONS ###############
-input_path = "/home/seraj/DNAproj/Pilot-Nitsan/seq.txt"
+input_path = "/home/seraj/DNAproj/Omer-Pilot-Dataset/allseqmerged.fastq"
 output_path = "/home/seraj/DNAproj/output/removeprimers_and_rev_com_primers/reads_filter_without_primers_rev_com.fastq" 
 output_path_primers = "/home/seraj/DNAproj/output/removeprimers_and_rev_com_primers/reads_filter_with_primers_rev_com.fastq"
 front_primer = "TCGTCGGCAGCGTCAGATGTGTATAAGAGACAG"
 back_primer = "CTGTCTCTTATACACATCTCCGAGCCCACGAGAC"
+datalen = 140
 # front_primer ="TAAGAGACAG"
 # back_primer = "CTGTCTCTTA"
 
@@ -72,6 +73,10 @@ def check_ED(line, front_ind, back_ind, front_ball, back_ball):
             if front_ind != -1:
                 break
 
+    if front_ind == -1:
+        return [front_ind, back_ind]
+    
+    
     if back_ind == -1:
         for edited_primer in back_ball[0]:
             back_ind = line.find(edited_primer)
@@ -91,6 +96,25 @@ def check_ED(line, front_ind, back_ind, front_ball, back_ball):
                 break
     
     return [front_ind, back_ind]
+
+
+def edit_distance(string1, string2):
+    if len(string1) > len(string2):
+        difference = len(string1) - len(string2)
+        string1[:difference]
+
+    elif len(string2) > len(string1):
+        difference = len(string2) - len(string1)
+        string2[:difference]
+
+    else:
+        difference = 0
+
+    for i in range(len(string1)):
+        if string1[i] != string2[i]:
+            difference += 1
+
+    return difference 
 
 ############### **************** ###############
 
@@ -200,7 +224,7 @@ def find_comp_rev_OMER():
             seq_with_primers = line[front_ind:back_ind + len(back_primer)]
             seq_without_primers = line[front_ind + len(front_primer): back_ind]
             
-            if abs(len(seq_without_primers) - 140) <=5: # we take data in offset 5 at most ,else throw it.
+            if abs(len(seq_without_primers) - 140) <=10: # we take data in offset 5 at most ,else throw it.
                 file_output_with_primers.write(seq_with_primers + "\n")
                 file_output_without_primers.write(seq_without_primers + "\n")
     print(counter)
@@ -339,7 +363,7 @@ def find_ED():
     file_output_without_primers.close()
 
 
-def find_comp_rev_ED():
+def find_comp_rev_oneED():
     file_input_fastq =open(input_path, "r")
     file_output_with_primers = open(output_path_primers, "w")
     file_output_without_primers = open(output_path, "w")
@@ -352,6 +376,7 @@ def find_comp_rev_ED():
     back_sub_ball = get_substitution_ball(back_primer)
     front_ball = [front_del_ball,front_ins_ball, front_sub_ball]
     back_ball = [back_del_ball, back_ins_ball, back_sub_ball]
+    print(len(back_ball[0]) + len(back_ball[1]) + len(back_ball[2]) )
     
     lines = file_input_fastq.readlines()
     for line in lines:
@@ -393,6 +418,58 @@ def find_comp_rev_ED():
 
 ############### **************** ###############
 
+
+def find_comp_rev_aproxemationED():
+    file_input_fastq =open(input_path, "r")
+    file_output_with_primers = open(output_path_primers, "w")
+    file_output_without_primers = open(output_path, "w")
+    
+    
+    lines = file_input_fastq.readlines()
+    for line in lines:
+        front_ind = line.find(front_primer)
+        back_ind = line.find(back_primer)
+    #_______________________________________________________#
+        if front_ind == -1:
+            # if couldn't find, search for primers with ~ED <= 5
+            for i in range(len(line) - datalen - len(back_primer) - len(front_primer) + 5) :
+                if edit_distance(line[i : i + len(front_primer)], front_primer) < 6:
+                    front_ind = i
+        if back_ind == -1 and front_ind != -1:
+            for i in range(front_ind + 130, front_ind + 150):
+                if edit_distance(line[i : i + len(back_primer)], back_primer) < 6:
+                    back_ind = i
+        
+        # if couldn't find, search for complementary
+        if front_ind == -1 or back_ind == -1:
+            line_com = copy_compliment(line)
+            line_rev_com = copy_reverse(line_com)
+            front_ind = line_rev_com.find(front_primer)
+            back_ind = line_rev_com.find(back_primer)
+            line = line_rev_com
+            # if front_ind == -1 or back_ind == -1:
+            #     indices = check_ED(line, front_ind, back_ind, front_ball, back_ball)
+            #     front_ind = indices[0]
+            #     back_ind = indices[1]
+    #_______________________________________________________#
+
+        if front_ind != -1 and back_ind != -1:
+            seq_with_primers = line[front_ind:back_ind + len(back_primer)]
+            seq_without_primers = line[front_ind + len(front_primer): back_ind]
+            
+            if abs(len(seq_without_primers) - 140) <=5: # we take data in offset 5 at most ,else throw it.
+                file_output_with_primers.write(seq_with_primers + "\n")
+                file_output_without_primers.write(seq_without_primers + "\n")
+
+    file_input_fastq.close()
+    file_output_with_primers.close()
+    file_output_without_primers.close()
+
+
+
+############### **************** ###############
+
+
 ###############  MAIN FUNCTION   ###############
 
 
@@ -401,8 +478,9 @@ def find_comp_rev_ED():
 # find_rev() # Run Time -> 15 sec
 # find_comp() # Run Time -> 59.5 sec
 # find_comp_rev() # Run Time -> 66 sec
-find_comp_rev_OMER()
-# find_comp_rev_ED()
+# find_comp_rev_OMER()
+# find_comp_rev_oneED()
+find_comp_rev_aproxemationED()
 
 
 
